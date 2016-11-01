0000eb8bb6db06f4c3ca749da5287b313b288d40
Shannon Riester <shannonriester@gmail.com>
2016-11-01 18:40:27 -0500

    added styles to default image on modal

--- app/scripts/Components/ModalHeader.js
+++ app/scripts/Components/ModalHeader.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
+import store from '../store';
+
 export default React.createClass({
   render() {
     let greeting = (<p className="greeting">Hi, Welcome to Simon!</p>);
-    if (this.props.username) {
-      greeting = (<p className="greeting">Hi, {this.props.username}!</p>);
+    let url = '/assets/Simon.png';
+
+    if (localStorage.authtoken !== store.anon.authtoken) {
+      greeting = (<p className="greeting">Hi, {this.props.session.username}!</p>);
+      url = this.props.session.profilePic;
     }
 
     return (
       <header className="modal-header-component">
         <div className="greeting-container">
-          <figure className="profile-pic" style={{backgroundImage:`url(${this.props.session.profilePic})`}} alt="profile picture"></figure>
+          <figure className="profile-pic" style={{backgroundImage:`url(${url})`}} alt="profile picture"></figure>
           <figcaption>{greeting}</figcaption>
         </div>
       </header>
